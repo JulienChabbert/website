b6b024528e1daddb6549ce3ef98c0ccd95755bfe
Julien Chabbert <johndoe@example.com>
2024-04-15 09:44:54 +0200

    recuperation

--- src/screens/Ressources.tsx
+++ src/screens/Ressources.tsx
@@ -1,5 +1,6 @@
 import { PiLink } from 'react-icons/pi'
 
+
 export const Ressources = () => {
     return (
         <main>
@@ -8,6 +9,7 @@ export const Ressources = () => {
             <ul>
                 <li className="flex items-center"><PiLink className="mr-3"/><a className="hover:underline" href="https://Python.org">Telecharger Python</a></li>
                 <li className="flex items-center"><PiLink className="mr-3"/><a className="hover:underline" href="https://github.com">Telecharger Pack ressources pygame</a></li>
+                <li className="flex items-center"><PiLink className="mr-3"/><a className="hover:underline" href="game.html">Jouer a Snake</a></li>
             </ul>
         </main>
     )
