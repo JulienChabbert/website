79c228955524ef108e1cba9889daaf67562a5c9b
Julien Chabbert <johndoe@example.com>
2024-05-17 11:14:52 +0200

    new save

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 export const About = () => {
     return(
         <section className="mb-5">
-            <h1 className="text-3xl font-extrabold">Bienvenue</h1>
-            <p className="mt-5">Booh2 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, quas quasi veritatis ratione 
+            <h1 className="text-3xl font-extrabold">Bienvenue 56</h1>
+            <p className="mt-5">Booh456 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, quas quasi veritatis ratione 
             ipsam quisquam quibusdam? Ipsum vero unde necessitatibus, soluta odit fugiat molestiae cum, iure
             facere nobis quibusdam corrupti!</p>
         </section>
